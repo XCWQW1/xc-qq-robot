d2f59a9a58b7810e1c0a1cb0a628db9d14db8432
XCWQW1 <3539757707@qq.com>
2023-06-06 00:11:17 +0800

    修复文件创建

--- main.py
+++ main.py
@@ -448,6 +448,8 @@ if __name__ == '__main__':
         @staticmethod
         def save_log(logs):
             log_sp = LogSP()
+            if not os.path.exists('logs'):
+                os.mkdir('logs')
             with open(f'logs/{LogSP.now_time_and_day_file}.log', 'a') as f_0:
                 f_0.write(f"{logs}\n")
 
