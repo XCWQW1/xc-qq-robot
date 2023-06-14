99bcc4435436d4d2eb7b47ada612b07e3448d1a7
Endermite <105129407+EndermiteX@users.noreply.github.com>
2023-06-15 01:47:21 +0800

    为QQApi添加注释

--- API/api_qq.py
+++ API/api_qq.py
@@ -25,6 +25,16 @@ class QQApi:
     # 发送消息
     @staticmethod
     def send(q_message_type, send_msg, auto_escape, q_group_id="", q_user_id="", q_user_name=""):
+        """
+        发送消息
+        @param q_message_type:group / private 消息类型
+        @param send_msg:要发送的内容
+        @param auto_escape:消息内容是否作为纯文本发送 ( 即不解析 CQ 码 )
+        @param q_group_id:群号
+        @param q_user_id:QQ 号
+        @param q_user_name:昵称
+        @return:
+        """
         from API.api_log import Log
         from API.api_thread import start_thread
         if q_message_type == "group":
@@ -48,7 +58,7 @@ class QQApi:
 
                 # 发送日志
                 start_thread(func=Log.send,
-                             args=(send_msg, q_message_type, q_group_id, q_message_id, q_group_name, q_user_id, ))
+                             args=(send_msg, q_message_type, q_group_id, q_message_id, q_group_name, q_user_id,))
 
             else:
                 start_thread(func=Log.error,
@@ -81,6 +91,11 @@ class QQApi:
     # 取群信息
     @staticmethod
     def get_group(q_group_id):
+        """
+        获取群名称
+        @param q_group_id:群号
+        @return:
+        """
         response = requests.get(url=cqhttp_url + f"get_group_info?group_id={q_group_id}").text
         json_send = json.loads(response)
         return json_send["data"]["group_name"]
@@ -88,12 +103,22 @@ class QQApi:
     # 撤回信息
     @staticmethod
     def del_msg(q_message_id):
+        """
+        撤回消息
+        @param q_message_id:消息 ID
+        @return:
+        """
         request = requests.get(url=cqhttp_url + f"delete_msg?message_id={q_message_id}").json()
         return request["status"]
 
     # 取用户信息
     @staticmethod
     def get_user_nickname(q_user_id):
+        """
+        获取用户昵称
+        @param q_user_id:QQ 号
+        @return:
+        """
         response = requests.get(url=cqhttp_url + f"get_stranger_info?user_id={q_user_id}&no_cache=true").text
         json_send = json.loads(response)
         q_user_name = json_send["data"]["nickname"]
@@ -101,8 +126,15 @@ class QQApi:
 
     @staticmethod
     def get_user_group_card(q_user_id, q_group_id):
+        """
+        获取群成员名片／备注
+        @param q_user_id:QQ 号
+        @param q_group_id:群号
+        @return:
+        """
         from API.api_thread import start_thread
-        response = requests.get(url=cqhttp_url + f"get_group_member_info?group_id={q_group_id}&user_id={q_user_id}&no_cache=false").text
+        response = requests.get(
+            url=cqhttp_url + f"get_group_member_info?group_id={q_group_id}&user_id={q_user_id}&no_cache=false").text
         json_send = json.loads(response)
         q_user_group_card = ""
         if json_send["data"]["card"]:
@@ -114,14 +146,35 @@ class QQApi:
     # 取运行状态
     @staticmethod
     def get_status():
+        """
+        获取Go-CqHttp状态
+        @return:
+        """
         response = requests.get(url=cqhttp_url + "get_status").json()
         return response
 
     @staticmethod
     def set_group_add_request(q_add_type, q_add_flag, q_add_approve, q_add_reason=''):
-        request = requests.get(url=cqhttp_url + f"set_group_add_request?flag={q_add_flag}&approve={q_add_approve}&type={q_add_type}&reason={q_add_reason}").json()
+        """
+        处理加群请求／邀请
+        @param q_add_type:请求类型
+        @param q_add_flag:加群请求的 flag
+        @param q_add_approve:是否同意请求／邀请
+        @param q_add_reason:拒绝理由
+        @return:
+        """
+        request = requests.get(
+            url=cqhttp_url + f"set_group_add_request?flag={q_add_flag}&approve={q_add_approve}&type={q_add_type}&reason={q_add_reason}").json()
         return request
 
     @staticmethod
     def set_group_ban(q_group_id, q_user_id, duration):
-        request = requests.get(url=cqhttp_url + f"set_group_ban?group_id={q_group_id}&user_id={q_user_id}&duration={duration}")
+        """
+        群单人禁言
+        @param q_group_id:群号
+        @param q_user_id:目标QQ 号
+        @param duration:禁言时长
+        @return:
+        """
+        request = requests.get(
+            url=cqhttp_url + f"set_group_ban?group_id={q_group_id}&user_id={q_user_id}&duration={duration}")
