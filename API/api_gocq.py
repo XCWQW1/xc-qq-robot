9f9155d9203f196d8574361153163812638b58f7
Endermite <105129407+EndermiteX@users.noreply.github.com>
2023-06-16 04:12:35 +0800

    go-cqhttp API

--- API/api_gocq.py
+++ API/api_gocq.py
@@ -68,7 +68,7 @@ class CQApi:
             @param user_id:对方 QQ 号 ( 消息类型为 private 时需要 )
             @param group_id:群号 ( 消息类型为 group 时需要 )
             @param message:要发送的内容
-            @param message_type:消息类型, 支持 private、group , 分别对应私聊、群组, 如不传入, 则根据传入的 *_id 参数判断，私聊优先
+            @param message_type:消息类型, 支持 private、group , 分别对应私聊、群组, 如不传入, 则根据传入的 *_id 参数判断
             @param auto_escape:	消息内容是否作为纯文本发送 ( 即不解析 CQ 码 ) , 只在 message 字段是字符串时有效
             """
             if message_type == "" and user_id is not None and group_id is None:
